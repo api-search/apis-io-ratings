b0e74c0f2a1f143fef1db9e0717247781b6b6a69
Kin Lane <kinlane@gmail.com>
2023-08-12 15:49:25 -0400

    update swagger pull

--- paths/ratings/apisjson/apis/properties/put/index.js
+++ paths/ratings/apisjson/apis/properties/put/index.js
@@ -1,5 +1,13 @@
 const vandium = require('vandium');
 const mysql  = require('mysql');
+const https  = require('https');
+const AWS = require("aws-sdk");
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.key,
+  secretAccessKey: process.env.secret, 
+  Bucket: "kinlane-productions2"
+});
 
 exports.handler = vandium.generic()
   .handler( (event, context, callback) => {
@@ -10,25 +18,115 @@ exports.handler = vandium.generic()
     password : process.env.password,
     database : process.env.database
     });
-    
-    var total_properties = Object.keys(event.body).length;
+     
+    let currentDate = new Date();
+    let startDate = new Date(currentDate.getFullYear(), 0, 1);
+    let days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+     
+    const weekNumber = Math.ceil(days / 7);
 
-    var sql = 'UPDATE apis SET ';
+    var sql = "SELECT * FROM apisjson WHERE pulled <> 0 AND rated <> " + weekNumber + " AND status = 200 AND (type LIKE '%swagger%' OR type LIKE '%OPENAPI%' LIMIT 1";
+    connection.query(sql, function (error, results, fields) { 
+      
+      if(results && results.length > 0){
+        
+        // Pull any new ones.
+        var apisjson_name = results[0].name;
+        var apisjson_url = results[0].url;
+        var apisjson_slug = results[0].slug;
+
+        var local_openapi = "https://kinlane-productions2.s3.amazonaws.com/" + results[0].path;
+        console.log(local_openapi);
+
+        https.get(local_openapi, res => {
+          
+          let data = [];
+          //const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+          
+          //console.log('Status Code:', res.statusCode);
+          //console.log('Date in Response header:', headerDate);
+        
+          res.on('data', chunk => {
+            data.push(chunk);
+          });
+        
+          res.on('end', () => {
+
+            const options = {
+                hostname: 'iuwhp1w2ha.execute-api.us-east-1.amazonaws.com',
+                method: 'POST',
+                path: '/staging/linter/apisjson',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': process.env.api_key
+                }
+            };
+
+            console.log(options);
+
+            //var apisjson = JSON.stringify(Buffer.concat(data).toString());
+            console.log(Buffer.concat(data).toString());                     
+        
+            var req = https.request(options, (res) => {
+
+                let body = '';
+                res.on('data', (chunk) => {
+                    body += chunk;
+                });
     
-    var property_count = 1;
-    for (const [key, value] of Object.entries(event.body)) {
-      sql += key + " = '" + value + "'";
-      if(property_count != total_properties){
-        sql += ',';
+                res.on('end', () => {
+
+                    var spectral_results = JSON.parse(body);
+                    console.log(spectral_results);
+
+                    var rules = '';
+                    for (let i = 0; i < spectral_results.length; i++) {
+                      if(!rules.includes(spectral_results[i].code)){
+                        rules += spectral_results[i].code + ",";
+                      }
+                    }
+                    rules = rules.substring(0, rules.length - 1);
+
+                    var sql = "UPDATE apisjson SET rated = " + weekNumber + ", rules = '" + rules + "' WHERE url = '" + apisjson_url + "'";
+                    connection.query(sql, function (error, results, fields) { 
+                      var response = {};
+                      response.message = "Rated " + apisjson_name + " APIs.json";
+                      response.rules = rules;
+                      callback( null, response);
+                      connection.end();
+                    }); 
+
+                });
+
+                res.on('error', () => {
+                  callback( null, "Error pulling from S3." );
+                  callback( null, res.statusCode);
+                  connection.end();
+                });
+
+            });
+
+            req.write(Buffer.concat(data).toString());
+            req.end(); 
+
+          });
+        }).on('error', err => {
+          callback( null, err );
+          connection.end();
+        });
+        
+
       }
-      property_count++;
-    }
-
-    sql += " WHERE id = " + connection.escape(event.api_id);
+      else{
+        
+        // Pull one that is old
+        var response = {};
+        response['pulling'] = "No more new ones, looking for old ones.";            
+        callback( null, error );  
+        connection.end();        
+        
+      }
+      
+    });  
   
-    connection.query(sql, function (error, results, fields) {
-
-    callback( null );
-
-  });
 });
